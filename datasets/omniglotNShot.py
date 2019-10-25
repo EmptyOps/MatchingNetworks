94f361345066d5603c3456c7dca6b681ed787ee6
hiren <hi0001234d@gmail.com>
2019-10-25 11:51:42 +0530

    benchmark 2

--- datasets/omniglotNShot.py
+++ datasets/omniglotNShot.py
@@ -79,7 +79,7 @@ class OmniglotNShotDataset():
                     if len( temp[input_labels[i]] ) >= 30:  #only 20 samples per class
                         if True or input_labels[i] == 6:
                             if input_labels[i] in temp_to_be_predicted:
-                                if len( temp_to_be_predicted[input_labels[i]] ) >= 30:  #only 20 samples per class
+                                if len( temp_to_be_predicted[input_labels[i]] ) >= 0:  #only 20 samples per class
                                     continue
                                 
                                 temp_to_be_predicted[input_labels[i]].append( input[i][:,:,np.newaxis] )
