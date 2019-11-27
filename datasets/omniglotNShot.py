a9c933efd4a9a37e5b56255d5b80f88334178583
hiren <hi0001234d@gmail.com>
2019-11-27 15:49:43 +0530

    benchmark 2

--- datasets/omniglotNShot.py
+++ datasets/omniglotNShot.py
@@ -83,6 +83,9 @@ class OmniglotNShotDataset():
                 #if is_evaluation_only == True and input_labels[i] >= self.prediction_classes:
                 #    continue
 
+                if input_labels[i] >= self.total_base_classes:
+                    continue
+                
                 if input_labels[i] in temp:
                     if len( temp[input_labels[i]] ) >= 19:  #only 20 samples per class
                         if is_evaluation_only == False and (input_labels[i] < self.total_base_classes or np.mod( input_labels[i] - self.total_base_classes, 30 ) == 0 or np.mod( input_labels[i] - (self.total_base_classes+1), 30 ) == 0):            #True or False and (True or input_labels[i] == 6):
