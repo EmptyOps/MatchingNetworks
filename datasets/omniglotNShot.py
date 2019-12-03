2176441ec72aa2b8b0ca9b694a6119c0b6b84e13
hiren <hi0001234d@gmail.com>
2019-12-03 20:42:52 +0530

    benchmark 2

--- datasets/omniglotNShot.py
+++ datasets/omniglotNShot.py
@@ -26,7 +26,7 @@ PiLImageResize = lambda x: x.resize((28,28))
 np_reshape = lambda x: np.reshape(x, (28, 28, 1))
 
 class OmniglotNShotDataset():
-    def __init__(self, dataroot, batch_size = 100, classes_per_set=10, samples_per_class=1, is_use_sample_data = True, input_file="", input_labels_file="", total_input_files=-1, is_evaluation_only = False, evaluation_input_file = "", evaluation_labels_file = "", evaluate_classes = 1, is_eval_with_train_data = 0, negative_test_offset = 0):
+    def __init__(self, dataroot, batch_size = 100, classes_per_set=10, samples_per_class=1, is_use_sample_data = True, input_file="", input_labels_file="", total_input_files=-1, is_evaluation_only = False, evaluation_input_file = "", evaluation_labels_file = "", evaluate_classes = 1, is_eval_with_train_data = 0, negative_test_offset = 0, is_apply_pca_first = 0, cache_samples_for_evaluation = 100):
 
         if is_evaluation_only == False:
             np.random.seed(2191)  # for reproducibility
@@ -287,6 +287,70 @@ class OmniglotNShotDataset():
         """
         
         self.shuffle_classes = []
+        self.is_apply_pca_first = is_apply_pca_first
+        
+        #pca 
+        if self.is_apply_pca_first == 1:
+            #
+            print("pca matlab")
+            from matplotlib.mlab import PCA
+
+            data = np.array(np.random.randint(10,size=(10,3)))
+            print( PCA(self.x) )
+
+            #
+            print( "pca custom from so https://stackoverflow.com/a/13224592" )
+            def PCA(data, dims_rescaled_data=2):
+                """
+                returns: data transformed in 2 dims/columns + regenerated original data
+                pass in: data as 2D NumPy array
+                """
+                import numpy as NP
+                from scipy import linalg as LA
+                m, n = data.shape
+                # mean center the data
+                data -= data.mean(axis=0)
+                # calculate the covariance matrix
+                R = NP.cov(data, rowvar=False)
+                # calculate eigenvectors & eigenvalues of the covariance matrix
+                # use 'eigh' rather than 'eig' since R is symmetric, 
+                # the performance gain is substantial
+                evals, evecs = LA.eigh(R)
+                # sort eigenvalue in decreasing order
+                idx = NP.argsort(evals)[::-1]
+                evecs = evecs[:,idx]
+                # sort eigenvectors according to same index
+                evals = evals[idx]
+                # select the first n eigenvectors (n is desired dimension
+                # of rescaled data array, or dims_rescaled_data)
+                evecs = evecs[:, :dims_rescaled_data]
+                # carry out the transformation on the data using eigenvectors
+                # and return the re-scaled data, eigenvalues, and eigenvectors
+                return NP.dot(evecs.T, data.T).T, evals, evecs
+
+            def test_PCA(data, dims_rescaled_data=2):
+                '''
+                test by attempting to recover original data array from
+                the eigenvectors of its covariance matrix & comparing that
+                'recovered' array with the original data
+                '''
+                _ , _ , eigenvectors = PCA(data, dim_rescaled_data=2)
+                data_recovered = NP.dot(eigenvectors, m).T
+                data_recovered += data_recovered.mean(axis=0)
+                assert NP.allclose(data, data_recovered)
+
+
+            def plot_pca(data):
+                from matplotlib import pyplot as MPL
+                clr1 =  '#2026B2'
+                fig = MPL.figure()
+                ax1 = fig.add_subplot(111)
+                data_resc, data_orig = PCA(data)
+                ax1.plot(data_resc[:, 0], data_resc[:, 1], '.', mfc=clr1, mec=clr1)
+                MPL.show()
+
+            print( plot_pca(self.x) )
+            
         
         #TODO temp
         #for variational testing 
@@ -334,7 +398,7 @@ class OmniglotNShotDataset():
         else:
             self.is_rotate = False
             self.cache_sample = 300
-            self.cache_sample_prediction = 300  #90
+            self.cache_sample_prediction = cache_samples_for_evaluation  
             if is_evaluation_only == False:
                 #self.x_train, self.x_test, self.x_val  = self.x[:900], self.x[900:1200], self.x[1200:]
                 self.x_train, self.x_test, self.x_val  = self.x[:30], self.x[30:43], self.x[43:]
--- mainOmniglot.py
+++ mainOmniglot.py
@@ -87,7 +87,7 @@ data = omniglotNShot.OmniglotNShotDataset(dataroot=dataroot, batch_size = batch_
                                           samples_per_class=samples_per_class, 
                                           is_use_sample_data=is_use_sample_data, input_file=sys.argv[2], input_labels_file=sys.argv[3], 
                                           total_input_files = total_input_files, is_evaluation_only = is_evaluation_only, 
-                                          evaluation_input_file = sys.argv[8], evaluation_labels_file = sys.argv[14], evaluate_classes = int(sys.argv[25]), is_eval_with_train_data = int(sys.argv[26]), negative_test_offset = int(sys.argv[27]))
+                                          evaluation_input_file = sys.argv[8], evaluation_labels_file = sys.argv[14], evaluate_classes = int(sys.argv[25]), is_eval_with_train_data = int(sys.argv[26]), negative_test_offset = int(sys.argv[27]), is_apply_pca_first = int(sys.argv[29]), cache_samples_for_evaluation = int(sys.argv[30]))
 
 obj_oneShotBuilder = OneShotBuilder(data,model_path=model_path)
 obj_oneShotBuilder.build_experiment(batch_size, classes_per_set, samples_per_class, channels, fce)
