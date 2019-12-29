b7fbd0eb811a63c8a4eed6c6b2da701a126fd236
hiren <hi0001234d@gmail.com>
2019-12-29 20:31:52 +0530

    benchmark 12

--- models/MatchingNetwork.py
+++ models/MatchingNetwork.py
@@ -380,7 +380,7 @@ class MatchingNetwork(nn.Module):
                                     print( values )
                                     print( indices.squeeze() )
                                     print( target_label[:,i] )
-                                    print( "accuracy found above limitttttttttttttttttttttttttttttttttttttttt " + str( torch.mean((indices.squeeze() == target_label[:,i]).float()) ) )
+                                    print( ".................loss found below limitttttttttttttttttttttttttttttttttttttttt " + str(F.cross_entropy(preds, target_label[:,i].long())))
                                     
                                 if False and torch.mean((indices.squeeze() == target_label[:,i]).float()) >= 0.9:
                                     print( "accuracy found above limitttttttttttttttttttttttttttttttttttttttt " + str( torch.mean((indices.squeeze() == target_label[:,i]).float()) ) )
