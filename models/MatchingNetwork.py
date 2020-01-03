3a555aa6d9fe7e7ab03b0c0ff8590a1854afd0a7
hiren <hi0001234d@gmail.com>
2020-01-03 15:22:29 +0530

    benchmark 13

--- models/MatchingNetwork.py
+++ models/MatchingNetwork.py
@@ -118,14 +118,14 @@ class MatchingNetwork(nn.Module):
             try:
                 logs = array( json.load( open( log_file_encoded ) ) ) 
                 for eimg in encoded_images:
-                    logs = np.concatenate( ( logs, np.array( eimg.cpu().numpy() ) ), axis=0 )
+                    logs = np.concatenate( ( logs, np.array( eimg.cpu().detach().numpy() ) ), axis=0 )
             except Exception as e:
                 logs = None
                 for eimg in encoded_images:
                     if not logs == None:
-                        logs = np.concatenate( ( logs, np.array( eimg.cpu().numpy() ) ), axis=0 )
+                        logs = np.concatenate( ( logs, np.array( eimg.cpu().detach().numpy() ) ), axis=0 )
                     else:
-                        logs = np.array( eimg.cpu().numpy() )
+                        logs = np.array( eimg.cpu().detach().numpy() )
                 
             #save
             with open( log_file_encoded, 'w') as outfile:
@@ -193,9 +193,9 @@ class MatchingNetwork(nn.Module):
                 # load logged array and append and save
                 try:
                     logs = array( json.load( open( log_file_similarities ) ) ) 
-                    logs = np.concatenate( ( logs, np.array( similarities.cpu().numpy() ) ), axis=0 )
+                    logs = np.concatenate( ( logs, np.array( similarities.cpu().detach().numpy() ) ), axis=0 )
                 except Exception as e:
-                    logs = np.array( similarities.cpu().numpy() )
+                    logs = np.array( similarities.cpu().detach().numpy() )
                     
                 #save
                 with open( log_file_similarities, 'w') as outfile:
