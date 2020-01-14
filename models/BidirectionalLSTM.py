60af8634c4bca85fab49090da432d2d3e72cce31
hiren <hi0001234d@gmail.com>
2020-01-14 23:23:44 +0530

    benchmark 14

--- models/BidirectionalLSTM.py
+++ models/BidirectionalLSTM.py
@@ -57,10 +57,16 @@ class BidirectionalLSTM(nn.Module):
         :param x: The inputs should be a list of shape [sequence_length, batch_size, 64]
         :return: Returns the LSTM outputs, as well as the forward and backward hidden states.
         """
-        #c0 = Variable(torch.rand(self.lstm.num_layers*2, self.batch_size, self.lstm.hidden_size),requires_grad=False).cuda()
-        c0 = Variable(torch.rand(self.lstm.num_layers*2, 1, self.lstm.hidden_size),requires_grad=False).cuda()
-        #h0 = Variable(torch.rand(self.lstm.num_layers*2, self.batch_size, self.lstm.hidden_size),requires_grad=False).cuda()
-        h0 = Variable(torch.rand(self.lstm.num_layers*2, 1, self.lstm.hidden_size),requires_grad=False).cuda()
+        if torch.cuda.is_available():
+            #c0 = Variable(torch.rand(self.lstm.num_layers*2, self.batch_size, self.lstm.hidden_size),requires_grad=False).cuda()
+            c0 = Variable(torch.rand(self.lstm.num_layers*2, 1, self.lstm.hidden_size),requires_grad=False).cuda()
+            #h0 = Variable(torch.rand(self.lstm.num_layers*2, self.batch_size, self.lstm.hidden_size),requires_grad=False).cuda()
+            h0 = Variable(torch.rand(self.lstm.num_layers*2, 1, self.lstm.hidden_size),requires_grad=False).cuda()
+        else:
+            #c0 = Variable(torch.rand(self.lstm.num_layers*2, self.batch_size, self.lstm.hidden_size),requires_grad=False).cuda()
+            c0 = Variable(torch.rand(self.lstm.num_layers*2, 1, self.lstm.hidden_size),requires_grad=False)
+            #h0 = Variable(torch.rand(self.lstm.num_layers*2, self.batch_size, self.lstm.hidden_size),requires_grad=False).cuda()
+            h0 = Variable(torch.rand(self.lstm.num_layers*2, 1, self.lstm.hidden_size),requires_grad=False)
         #print("c0 ", c0.shape, " h0 ", h0.shape)
         output, (hn, cn) = self.lstm(inputs, (h0, c0))
         return output, hn, cn
