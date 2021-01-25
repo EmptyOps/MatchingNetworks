d68b3fbd18ff9d411a9d15dbba8bd58cbbed7f30
hiren <hi0001234d@gmail.com>
2021-01-25 21:12:28 +0530

    benchmark 15

--- datasets/omniglotNShot.py
+++ datasets/omniglotNShot.py
@@ -926,6 +926,7 @@ class OmniglotNShotDataset():
                             #print( "example_inds here 1 " + str(n_test_samples) )
                     else:
                         print( "example_inds here 2 cur_class ", cur_class, " tvt_records_fall_short_clss ", self.tvt_records_fall_short_clss[cur_class], " samples_per_class ", self.samples_per_class )
+                        print( self.tvt_records_fall_short_clss )
                         #example_inds = np.random.choice(data_pack.shape[1], self.samples_per_class, False)
                         example_inds = np.random.choice(self.tvt_records_fall_short_clss[cur_class], self.samples_per_class, False)
 
